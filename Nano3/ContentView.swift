8c32f9a2b2fd9248c9b1eddb60a3dd41f0a8433d
JPeeeeee <joaopedrodev8@gmail.com>
2023-09-25 09:28:10 -0300

    Initial Commit

---
+++ Nano3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nano3
+//
+//  Created by João Pedro Vieira Santos on 25/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
